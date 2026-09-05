c4ffd4995bb9bcd9457b37fa3ecc117def584e2e
Farbod <Farboddaneshmandfard@gmail.com>
2026-09-05 03:51:15 +0330

    rate limiter put on login

---
+++ src/middlewares/rateLimiter.middleware.ts
@@ -0,0 +1,14 @@
+import { rateLimit } from 'express-rate-limit'
+
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  message: {
+    success: false,
+    message: 'Too many login attempts. Please try again later.',
+  },
+})
+
+export default loginLimiter
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import authController from '../controllers/auth.controller.ts'
 import protect from '../middlewares/auth.middleware.ts'
+import loginLimiter from '../middlewares/rateLimiter.middleware.ts'
 
 export const authRouter = express.Router()
 
 authRouter.post('/forgot-password', authController.forgot)
 authRouter.post('/reset-password/:token', authController.reset)
-authRouter.post('/login', authController.login)
+authRouter.post('/login', loginLimiter, authController.login)
 authRouter.post('/logout', authController.logout)
 authRouter.get('/me', protect, authController.getMe)
