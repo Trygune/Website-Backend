16ccddc52267b2a7a39670a19a66e11980560549
Farbod <Farboddaneshmandfard@gmail.com>
2026-09-06 05:50:16 +0330

    incresing rate limiter

--- src/middlewares/rateLimiter.middleware.ts
+++ src/middlewares/rateLimiter.middleware.ts
@@ -2,7 +2,7 @@ import { rateLimit } from 'express-rate-limit'
 
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  limit: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
   standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
   message: {
@@ -13,7 +13,7 @@ const loginLimiter = rateLimit({
 
 const contactLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  limit: 3,
+  limit: 5,
   standardHeaders: 'draft-8',
   legacyHeaders: false,
   message: {
